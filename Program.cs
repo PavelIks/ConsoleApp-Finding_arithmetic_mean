01523570715bdd54c428cbcc5d600333c11f89e3
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-28 13:03:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FindingArithmeticMean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                sum = sum + i * 2;
+            }
+            Console.WriteLine($"Cреднее арифметическое - {sum / 20}");
+
+            Console.ReadLine();
+        }
+    }
+}
